b7c98c91fedb5dd2b1db8842260dc5c23a14e276
MasCair2k16 <gotgame6@msn.com>
2022-02-15 18:29:55 -0800

    fix: add middleware

--- pages/api/plants.js
+++ pages/api/plants.js
@@ -6,9 +6,25 @@ const cors = Cors({
     methods: ['GET', 'HEAD'],
   })
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
+
 export default async function handler(req, res) {
 
-    await runMiddleware(req, res, cors); 
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+
     const id = await createPlant(req.body);
     res.status(200).json({ id });
 }
