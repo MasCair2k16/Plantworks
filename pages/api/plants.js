760ed3d8cf34da573db92e9b5e8cf060af801f18
MasCair2k16 <gotgame6@msn.com>
2022-02-15 16:50:16 -0800

    fix: more methods and Cors functionality

--- pages/api/plants.js
+++ pages/api/plants.js
@@ -1,6 +1,14 @@
 import { createPlant } from "../../lib/redis";
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+  })
 
 export default async function handler(req, res) {
+
+    await runMiddleware(req, res, cors); 
     const id = await createPlant(req.body);
     res.status(200).json({ id });
 }
