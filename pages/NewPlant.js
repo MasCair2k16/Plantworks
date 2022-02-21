91d90797a1cfadd278608ba65ce12324b88551e9
MasCair2k16 <gotgame6@msn.com>
2022-02-21 15:15:26 -0800

    feat: new Plant layout and  alert

--- pages/NewPlant.js
+++ pages/NewPlant.js
@@ -1,64 +1,105 @@
-import React, { useEffect } from 'react';
-// import { firestore } from "../firebase"
-// import {
-//   BrowserRouter,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
-import {Form, Button} from 'react-bootstrap';
-import { handleFetchPosts } from '../lib/PlantForm';
+import React, { useState, useEffect } from 'react';
+import {Form, Button, Alert} from 'react-bootstrap';
 import Style from '../styles/NewPlant.module.css';
+import NavigationBar from '../components/NavigationBar'
 
 function NewPlant() {
-    return (
 
-        <>
+    const [alert, setAlert] = useState(false)
+    const [response, setResponse] = useState('200')
+
+    useEffect(() => {
+        console.log("RESPONSE CODE", response);
+    }, [alert, response])
+
+    const handleFetchPosts = async (event) => {
+
+        event.preventDefault(); // submit without reloading the page
+        const form = new FormData(event.target);
+        const formData = Object.fromEntries(form.entries());
+    
+        console.log(formData);
+        let url = `${process.env.NEXT_PUBLIC_API_URL}/api/plants`;
+    
+        if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+            url = '/api/plants';
+    
+        const res = await fetch(url, {
+            body: JSON.stringify(formData),
+            headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'access-control-allow-methods': 'POST, GET, OPTIONS, PUT, DELETE',
+                'access-control-allow-origin': '*',
+            },
+            method: 'POST',
+        });
+
+        setAlert(true);
+        
+        if (res.status !== 200) {
+            console.log('Error: ' + res.statusText);
+        }
+
+        setResponse(res.status)
+        const result = await res.json();
+        console.log(result);
+        document.body.scrollTop = 0;
+    }
+
+    return (
         <div className={Style.container}>
-            <Form onSubmit={handleFetchPosts} className={Style.grid}>
-                <div className={Style.header}>
-                    <h1>Add a Plant</h1>
-                    <span>Thank you! You&apos;re helping many others how to care for their little Green friends!</span>
-                </div>
-                <hr />
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Name</Form.Label>
-                    <Form.Control type="text" name="name" placeholder="name" />
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Type</Form.Label>
-                    <Form.Control type="text" name="type" placeholder="type" />
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Description</Form.Label>
-                    <Form.Control type="text" name="description" placeholder="description" />
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Image</Form.Label>
-                    <Form.Control type="text" name="image" placeholder="image" />
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Water Frequency</Form.Label>
-                    <Form.Select type="text" name="waterFrequency" placeholder="water frequency" aria-label="Default select example">
-                        <option>Open this select menu</option>
-                        <option value="daily">Daily</option>
-                        <option value="weekly">Weekly</option>
-                        <option value="weekly">Bi-Weekly</option>
-                        <option value="monthly">Monthly</option>
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label>Plant Shade</Form.Label>
-                    <Form.Select type="text" name="shade" aria-label="Default select example">
-                        <option>Open this select menu</option>
-                        <option value="full sun">Full Sun</option>
-                        <option value="partial shade">Partial Shade</option>
-                        <option value="partial sun">Partial Sun</option>
-                    </Form.Select>
-                </Form.Group>
-                <Button variant="success" type="submit" className={Style.submitBtn}>Submit</Button>
-            </Form>
+            <NavigationBar />
+            <div className={Style.footer}>
+                {alert && <Alert variant={response === 200 ? 'success' : 'danger'} style={{marginBottom: '0'}}>
+                    {response === 200 ? "You've successfully created a new plant!" : 'There seems to be a problem, please try again later' }
+                </Alert>
+                }
+                <Form onSubmit={handleFetchPosts} className={Style.grid}>
+                    <div className={Style.header}>
+                        <h1>Add a Plant</h1>
+                        <span>Thank you! You&apos;re helping many others how to care for their little Green friends!</span>
+                    </div>
+                    <hr />
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Name</Form.Label>
+                        <Form.Control type="text" name="name" placeholder="name" />
+                    </Form.Group>
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Type</Form.Label>
+                        <Form.Control type="text" name="type" placeholder="type" />
+                    </Form.Group>
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Description</Form.Label>
+                        <Form.Control type="text" name="description" placeholder="description" />
+                    </Form.Group>
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Image</Form.Label>
+                        <Form.Control type="text" name="image" placeholder="image" />
+                    </Form.Group>
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Water Frequency</Form.Label>
+                        <Form.Select type="text" name="waterFrequency" placeholder="water frequency" aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="daily">Daily</option>
+                            <option value="weekly">Weekly</option>
+                            <option value="weekly">Bi-Weekly</option>
+                            <option value="monthly">Monthly</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-2">
+                        <Form.Label>Plant Shade</Form.Label>
+                        <Form.Select type="text" name="shade" aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="full sun">Full Sun</option>
+                            <option value="partial shade">Partial Shade</option>
+                            <option value="partial sun">Partial Sun</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Button variant="success" type="submit" className={Style.submitBtn}>Submit</Button>
+                </Form>
+            </div>
         </div>
-        </>
     );
 }
 
