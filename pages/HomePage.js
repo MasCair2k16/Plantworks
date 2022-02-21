ec6be3c71ed1ca0af99a911c7849eace80323be7
MasCair2k16 <gotgame6@msn.com>
2022-02-21 15:08:09 -0800

    feat: home page and searchplant page

---
+++ pages/HomePage.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+// import { firestore } from "../firebase"
+// import {
+//   BrowserRouter,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
+// import App from "../App";
+// import Calendar from "./Calendar"
+
+function HomePage() {
+
+  return (
+    <div>
+        <h1>HomePage</h1>
+        <p>Home page body content</p>
+    </div>
+    );
+}
+
+export default HomePage;
---
+++ pages/SearchPlant.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { Form, ListGroup } from 'react-bootstrap';
+import NavigationBar from '../components/NavigationBar'
+import Style from '../styles/SearchPlant.module.css';
+
+function SearchPlant() {
+
+    const [hits, setHits] = useState([]);
+
+    const Search = async (event) => {
+        const q = event.target.value;
+    
+        if (q.length > 2) {
+          const params = new URLSearchParams({ q });
+
+          let url = `${process.env.NEXT_PUBLIC_API_URL}/api/search?`;
+          let urlIndex = `${process.env.NEXT_PUBLIC_API_URL}/api/index`;
+
+          if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+              url = '/api/search?';
+              urlIndex = '/api/index';
+          
+          // TODO: check if sessions storage has called API INDEX!
+          try {
+            const indexRes = await fetch(urlIndex)
+          } catch (error) {
+            console.log("Index already called")
+          }
+
+          const res = await fetch(url + params);
+          const result = await res.json();
+          console.log(result);
+          setHits(result['plants']);
+        }
+      }
+    
+    return (
+        <div>
+          <NavigationBar />
+          <Form className={Style.grid}>
+          <div className={Style.header}>
+            <h1>Search a Plant</h1>
+            <span>To find a plant, enter the name or type and we&apos;ll find all the info for your little green friend!</span>
+          </div>
+          <hr />
+            <Form.Group className="mb-3">
+              <Form.Control onChange={Search} type="text" />
+            </Form.Group>
+          </Form>
+          { hits.map((hit) => (
+            <ListGroup horizontal={'sm'} className="my-2" key={hit.entityId}>
+              <ListGroup.Item>{hit.name}</ListGroup.Item>
+              <ListGroup.Item>{hit.type}</ListGroup.Item>
+              <ListGroup.Item>{hit.description}</ListGroup.Item>
+              <ListGroup.Item><image src={"../public/favicon.ico"} /></ListGroup.Item>
+              <ListGroup.Item>{hit.shade}</ListGroup.Item>
+              <ListGroup.Item>{hit.waterFrequency}</ListGroup.Item>
+            </ListGroup>
+          ))}
+      </div>
+    );
+}
+
+export default SearchPlant;
