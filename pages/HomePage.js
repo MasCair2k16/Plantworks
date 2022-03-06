6bfdbd7243c6dba73b0d1e2193371b20ce0db8f6
mcaird <gotgame6@msn.com>
2022-03-06 14:31:29 -0800

    Not fixed: better words and font

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
-// import { firestore } from "../firebase"
-// import {
-//   BrowserRouter,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
-// import App from "../App";
-// import Calendar from "./Calendar"
-
+import Style from '../styles/Home.module.css';
 function HomePage() {
 
   return (
-    <div>
-        <h1>HomePage</h1>
-        <p>Home page body content</p>
+    <div className={Style.homeContainer}>
+        <div>
+          <div className={Style.container}>
+            <h1 style={{FontFamily: 'Helvetica',}}>Discover <br></br> your plant's <br></br>  potential</h1>
+            <h1 style={{backgroundColor: 'grey'}}>IMAGE</h1>
+          </div>
+          <div className={Style.description}>
+            <span>Find or add a plant to help others </span>
+          </div>
+        </div>
+        <div style={{backgroundColor: 'green'}}>
+          <p>Chad description</p>
+        </div>
     </div>
     );
 }
