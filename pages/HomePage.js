771132c6807ee78455b027f25faf61f9d98d3136
MasCair2k16 <gotgame6@msn.com>
2022-03-08 20:45:59 -0800

    feat: adding step in home layout

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -6,15 +6,30 @@ function HomePage() {
     <div className={Style.homeContainer}>
         <div>
           <div className={Style.container}>
-            <h1 style={{FontFamily: 'Helvetica',}}>Discover <br></br> your plant's <br></br>  potential</h1>
+            <h1 className={Style.title}>Discover <br></br> your plant's <br></br>  potential</h1>
             <h1 style={{backgroundColor: 'grey'}}>IMAGE</h1>
           </div>
           <div className={Style.description}>
-            <span>Find or add a plant to help others </span>
+            <span>Needing to learn how to care for your plants has <br></br> never been this simple.</span>
           </div>
         </div>
-        <div style={{backgroundColor: 'green'}}>
-          <p>Chad description</p>
+        <div className={Style.steps}>
+          <div className={Style.stepBox}>
+            <h1>Step 1</h1>
+            <span>Find a plant that you like</span>
+          </div>
+          <div className={Style.stepBox}>
+            <h1>Step 2</h1>
+            <span>Learn how to care for your plant</span>
+          </div>
+          <div className={Style.stepBox}>
+            <h1>Step 3</h1>
+            <span>Enjoy your new plant</span>
+            </div>
+          <div className={Style.stepBox}>
+            <h1>Step 4</h1>
+            <span>Enjoy your new plant</span>
+          </div>
         </div>
     </div>
     );
