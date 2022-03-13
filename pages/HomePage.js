222bf766599acd449fc216e89ee06d07a7570253
mcaird <gotgame6@msn.com>
2022-03-13 16:43:12 -0700

    fix: fix deployment - silence StepBox component

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import Style from '../styles/Home.module.css';
-import navigationBar from '../components/navigationBar'
-import StepBox from '../components/StepBox'
+// import StepBox from '../components/StepBox'
 function HomePage() {
 
   return (
     <>
-    <navigationBar />
       <div className={Style.homeContainer}>
           <div>
             <div className={Style.container}>
@@ -18,10 +16,26 @@ function HomePage() {
             </div>
           </div>
           <div className={Style.steps}>
-              <StepBox stepTitle='Step 1' stepDesc='Search for a plant' />
+            <div className={Style.stepBox}>
+              <h1>Step 1</h1>
+              <span>Search for a plant</span>
+            </div>
+            <div className={Style.stepBox}>
+              <h1>Step 2</h1>
+              <span>Read the details about your plant</span>
+            </div>
+            <div className={Style.stepBox}>
+              <h1>Step 3</h1>
+              <span>Provide care for your plant</span>
+            </div>
+            <div className={Style.stepBox}>
+              <h1>Step 4</h1>
+              <span>And have your plant smiling :D</span>
+            </div>
+              {/* <StepBox stepTitle='Step 1' stepDesc='Search for a plant' />
               <StepBox stepTitle='Step 2' stepDesc='Read the details about your plant' />
               <StepBox stepTitle='Step 3' stepDesc='Provide care for your plant' />
-              <StepBox stepTitle='Step 4' stepDesc='And have your plant smiling :D' />
+              <StepBox stepTitle='Step 4' stepDesc='And have your plant smiling :D' /> */}
           </div>
       </div>
     </>
